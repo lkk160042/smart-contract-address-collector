c046c1f97d6b4bfff08c390c4dafa31ac3cc63a9
lkk160042 <diamondr1265@gmail.com>
2024-11-26 18:56:52 +0900

    lp 수집 함수 추가

---
+++ dex_info.py
@@ -0,0 +1,7 @@
+from contract_utils import Contract
+
+v2 = ["Uniswap v2", "PancakeSwap V2", "ShibaSwap", "DeFi Swap", "Orion", "Frax Swap", "EtherVista", "SmarDex", "MOODAR"]
+v3 = ["Uniswap v3", "PancakeSwap V3", "Solidly V3"]
+
+contract = Contract.from_url("http://192.168.10.12:9087")
+
